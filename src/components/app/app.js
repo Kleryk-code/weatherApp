99f5e176c5855e64c14cd8b918ad199a61bce1dd
Kleryk-code <realigorek@gmail.com>
2021-09-01 22:45:02 +0500

    Создал подем состояния

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,15 +6,48 @@ import OpenweathermapApi from '../../openweathermapApi/openweathermapApi';
 
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          searchRequest: '',
+          id: null,
+          cityName: '',
+          temp: null        
+        };
 
+        this.onChangeSearch = this.onChangeSearch.bind(this);
+        this.onSubmit = this.onSubmit.bind(this)
+    }
     weatherApi = new OpenweathermapApi();
+    
+
+    onSubmit(event) {
+        this.weatherApi.getCityData(this.state.searchRequest)
+          .then(data => this.setState({
+            id: data.id,
+            cityName: data.name,
+            temp: data.temp
+          }));
+          
+        event.preventDefault();
+      }
+
+    onChangeSearch(event) {
+        this.setState({searchRequest: event.target.value})
+    }   
     
 
     render() {
         return (
             <div>
-                <h2>Hello</h2>
-                <SearchForm />
+                <h2>Weather</h2>
+                <SearchForm 
+                    onSubmit = {this.onSubmit}
+                    //onChangeSearch = {this.onChangeSearch}
+                    //onSubmited = {() => console.log(1)}
+                    onChangeSearch = {this.onChangeSearch}
+                    state = {this.state}
+                    />
                 <WeatherCart />
             </div>
         );
--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -2,37 +2,46 @@ import React from 'react';
 import OpenweathermapApi from '../../openweathermapApi/openweathermapApi'
 
 class SearchForm extends React.Component {
-    constructor(props) {
+/*      constructor(props) {
       super(props);
-      this.state = {searchRequest: ''};
+      this.state = {
+        searchRequest: '',
+        id: null,
+        cityName: '',
+        temp: null        
+      };
 
       this.onChangeSearch = this.onChangeSearch.bind(this);
       this.onSubmit = this.onSubmit.bind(this)
     }
+
     weatherApi = new OpenweathermapApi();
 
     onSubmit(event) {
       this.weatherApi.getCityData(this.state.searchRequest)
-        .then(data => console.log(data));
-      // сохранить данные в стайт
-      //alert(this.state.searchRequest);
+        .then(data => this.setState({
+          id: data.id,
+          cityName: data.name,
+          temp: data.temp
+        }));
+      
       event.preventDefault();
     }
-
+    
     onChangeSearch(event) {
       this.setState({searchRequest: event.target.value})
       
-    }
+    } */
     
 
     render() {
       return (
         <div>
-          <form onSubmit = {this.onSubmit}>
+          <form onSubmit = {this.props.onSubmit}>
             <label> Search: 
               <input type="text" name="search" 
-                    value = {this.state.searchRequest}
-                    onChange = {this.onChangeSearch}
+                    value = {this.props.state.searchRequest}
+                    onChange = {this.props.onChangeSearch}
                     placeholder="City"/>
               <button type='submit'>Search</button>
             </label>
